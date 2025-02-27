5b9b132573a6ec782c1636af4f3a76de128092fa
C137Adrian <adriancanoespinosa@gmail.com>
2025-02-27 14:35:16 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  function numeraleat(array){
+    let nume = Math.floor(Math.random()*array.length);
+    return nume;
+  }
+  function excualeat(){
+    let numexcu = who[numeraleat(who)]+" "+action[numeraleat(action)]+" "+what[numeraleat(what)]+" "+when[numeraleat(when)];
+    return numexcu;
+  }
+  document.getElementById("excuse").innerText = excualeat();
 };
